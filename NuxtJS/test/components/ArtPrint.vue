435535194706dca2bb2db25416f7da23f98b7099
Job <job.lich666@gmail.com>
2021-08-09 14:46:19 -0500

    Initial commit

---
+++ NuxtJS/test/components/ArtPrint.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="mx-auto" max-width="480" elevation="5">
+    <v-img height="150" v-if="artInfo.thumbnail" :src="artInfo.thumbnail.lqip">
+    </v-img>
+    <v-img v-else src="https://i.stack.imgur.com/y9DpT.jpg" height="150">
+    </v-img>
+    <v-card-title>
+      {{ artInfo.title }}
+    </v-card-title>
+    <v-card-subtitle>
+      Place:
+      <span v-if="artInfo.place_of_origin">
+        {{ artInfo.place_of_origin }}
+      </span>
+      <span v-else> Unknown </span>
+    </v-card-subtitle>
+    <v-card-text>
+      Artist:
+      {{ artInfo.artist_display }}
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ['artInfo'],
+}
+</script>
---
+++ NuxtJS/test/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill height" fluid style="min-height: 480px">
+    <v-card>
+      <v-img
+        src="https://miro.medium.com/max/2000/1*9NJlqVUQpUbPm1ptkCKI0w.jpeg"
+        aspect-ratio="2"
+        contain
+      >
+      </v-img>
+    </v-card>
+    <v-card-title outlined class="mx-auto"> Art Gallery </v-card-title>
+    <v-row>
+      <v-col md="4" v-for="artWork in serverResponse.data" :key="artWork.id">
+        <ArtPrint :artInfo="artWork" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const serverResponse = await $axios.$get(
+      'https://api.artic.edu/api/v1/artworks?limit=10'
+    )
+    return {
+      serverResponse,
+    }
+  },
+}
+</script>
